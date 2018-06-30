077bd6e5d37e736727506860102981bdca393311
Penthious <tleffew1994@gmail.com>
2018-06-30 15:28:13 -0600

    deploy script

--- server-deploy.sh
+++ server-deploy.sh
@@ -5,3 +5,5 @@ mv Dockerfile.prod Dockerfile
 yarn docker:build
 yarn docker:push
 ssh root@178.128.72.185 "docker pull penthious/airbnb-clone:latest && docker tag penthious/airbnb-clone:latest dokku/airbnb:latest && dokku tags:deploy airbnb latest"
+mv Dockerfile Dockerfile.prod
+mv Dockerfile.dev Dockerfile
