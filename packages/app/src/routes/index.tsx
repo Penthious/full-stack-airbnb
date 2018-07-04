e2cb441b22f435707afdfa99f95f997af15530b7
Penthious <tleffew1994@gmail.com>
2018-07-04 08:50:57 -0600

    fixed login routing

--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { NativeRouter, Route, Switch } from 'react-router-native';
 
-import { LoginConnector } from "../modules/login/LoginConnector";
-// import { RegisterConnector } from '../modules/register/RegisterConnector'
+import { LoginConnector } from '../modules/login/LoginConnector';
+import { RegisterConnector } from '../modules/register/RegisterConnector';
 
 export const Routes = () => (
     <NativeRouter initialEntries={['/login']}>
         <Switch>
-            {/* <Route exact={true} patch='/register' component={RegisterConnector} /> */}
-            <Route exact={true} patch='/login' component={LoginConnector} />
+            <Route exact={true} path='/login' component={LoginConnector} />
+            <Route exact={true} path='/register' component={RegisterConnector} />
         </Switch>
     </NativeRouter>
-)
+);
