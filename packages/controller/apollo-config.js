e57c558013c40a321087231c8bc3cedf2014cd76
Penthious <tleffew1994@gmail.com>
2018-12-21 17:30:54 -0700

    updated to apollo 2

---
+++ packages/controller/apollo-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: "airbnb-clone",
+      //   url: "http://localhost:4000/graphql",
+      localSchemaFile: "./schema.json",
+    },
+  },
+};
