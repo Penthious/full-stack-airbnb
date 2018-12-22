618e0b9e9870a2b382b1dcc35af5ee1c647bb290
Penthious <tleffew1994@gmail.com>
2018-12-21 18:03:52 -0700

    upgrade rn

--- packages/app/rn-cli.config.js
+++ packages/app/rn-cli.config.js
@@ -1,6 +1,49 @@
-const getConfig = require("metro-bundler-config-yarn-workspaces");
+const blacklist = require("metro-config/src/defaults/blacklist");
+const getWorkspaces = require("get-yarn-workspaces");
 const path = require("path");
 
-module.exports = getConfig(__dirname, {
-  nodeModules: path.join(__dirname, "../..")
-});
+function getConfig(from, options = {}) {
+  const workspaces = getWorkspaces(from);
+
+  function getProjectRoots() {
+    return [
+      // Keep your project directory.
+      path.resolve(from),
+
+      // Include your forked package as a new root.
+      options.nodeModules || path.resolve(from, "../..", "node_modules"),
+    ].concat(workspaces);
+  }
+
+  const config = {
+    watchFolders: getProjectRoots(),
+    resolver: {
+      blacklistRE: blacklist(
+        workspaces.map(
+          workspacePath =>
+            `/${workspacePath.replace(
+              /\//g,
+              "[/\\\\]",
+            )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`,
+        ),
+      ),
+      extraNodeModules: {
+        "react-native": path.resolve(from, "node_modules/react-native"),
+      },
+    },
+  };
+
+  console.log("=-=================");
+  console.log("=-=================");
+  console.log("=-=================");
+  console.log("=-=================");
+
+  console.log(config);
+  console.log("=-=================");
+  console.log("=-=================");
+  console.log("=-=================");
+
+  return config;
+}
+
+module.exports = getConfig(__dirname);
